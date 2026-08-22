ca024d40e3e051f9b3a2ad8206e38b53bc2b7db3
v1570560-blip <v1570560@gmail.com>
2026-08-22 22:36:31 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bbuandbby.playlist',
+  appName: 'Bbu and bby',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
